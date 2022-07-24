96c13f7f889c53dbc31239e3d3b31af06a700df1
KordianJustek <biuro@omijus,com>
2022-07-24 21:27:58 +0200

    3 komit

--- dp/users/urls.py
+++ dp/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
-
-urlpatterns = [
-
-    path('register/', views.register, name='blog-register'),
-]
+#
+# urlpatterns = [
+#
+#     path('register/', views.register, name='blog-register'),
+# ]
